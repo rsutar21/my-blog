dcc9146e6a36fa0e605b8bd1c9305b2b178e3214
rsutar21 <rahulsutar21@gmail.com>
2020-03-15 17:43:42 +0530

    First Commit

---
+++ src/build/precache-manifest.cb41a5b98d7cac6cbb40d77df8bbf865.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78845b50babb9e94528aeffe176b835f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dc4257a42c7cdd00136",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "beff9bf8fd6d968ab93f",
+    "url": "/static/js/2.3605e02b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.3605e02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc4257a42c7cdd00136",
+    "url": "/static/js/main.1118a0cb.chunk.js"
+  },
+  {
+    "revision": "f8c88a4ab800e2a20047",
+    "url": "/static/js/runtime-main.ae3b9067.js"
+  }
+]);
